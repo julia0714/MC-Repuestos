112e7d40a019a19aa54158b1e453401af23b7ee8
Julian <julian07di@gmail.com>
2024-03-04 22:49:53 -0600

    login implementation

--- colaboradores.php
+++ colaboradores.php
@@ -12,27 +12,33 @@
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                     <div class="card c text-white" style="border-radius: 1rem;">
                         <div class="card-body p-5">
-
+                            <form action="" method="post">
                             <div class="mb-md-5 mt-md-4 pb-5">
-
+                        
                                 <h2 class="fw-bold mb-2 text-uppercase text-center">Colaborador</h2>
                                 <p class="text-white-50 text-center txt-second">¡Por favor, introduce tu usuario y contraseña para acceder!</p>
 
+
                                 <div class="form-outline form-white mb-4">
-                                    <label class="form-label" for="typeEmailX">Usuario</label>
-                                    <input type="email" id="typeEmailX" placeholder="Introduce aqui Usuario..." class="form-control form-control-lg" />
+                                    <label class="form-label" for="username">Usuario</label>
+                                    <input name="username" type="email" id="typeEmailX" placeholder="Introduce aqui Usuario..." class="form-control form-control-lg" />
                                 </div>
 
                                 <div class="form-outline form-white mb-4">
-                                    <label class="form-label" for="typePasswordX">Contraseña</label>
-                                    <input type="password" id="typePasswordX" placeholder="Introduce aqui Contraseña..." class="form-control form-control-lg" />
+                                    <label class="form-label" for="password">Contraseña</label>
+                                    <input name="password" type="password" id="typePasswordX" placeholder="Introduce aqui Contraseña..." class="form-control form-control-lg" />
                                 </div>
                                 <div class="d-flex justify-content-center align-items-center flex-column">
                                     <p class="small pb-lg-2 "><a class="fs-6 p-colors" href="#!">¿Has olvidado tu contraseña?</a></p>
-                                    <button class="btn btn-outline-light btn-lg px-5 btn-iniciar" type="submit">Iniciar</button>
+                                    <button  class="btn btn-outline-light btn-lg px-5 btn-iniciar" type="submit" name="btn_ingresar">Iniciar</button>
                                 </div>
+                                <?php
+                            include ("./layout/controlers/db_conexion.php");
+                            include ("./layout/controlers/login_controler.php");
+                            ?>
 
                             </div>
+                            </form>
 
                             <div>
                                 <p class="mb-0 text-center">¿No tienes una cuenta? <a href="#!" class="p-colors fw-bold">Crea una cuenta</a></p>
--- layout/controlers/login_controler.php
+++ layout/controlers/login_controler.php
@@ -2,6 +2,7 @@
 if (!empty($_POST["password"]) and !empty($_POST["username"])) {
     $user = $_POST["username"];
     $password = $_POST["password"];
+    
     $sql = $conn->query("select * from user where User ='$user' and Password ='$password'");
     if ($datos = $sql->fetch_object()) {
         header("location:index.php");
